45c970db297ee5750943982315cf47fa0ad45899
‘Juliano <julianoperins@gmail.com>
2020-06-02 01:36:24 -0400

    second commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        <TodoItem />
+      </div>
+    );
+  }
+}
+
+export default TodoList;
